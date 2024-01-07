4ad9212b35ee08adf68c8d81f9f2b02e27dccbfd
makhammadsoliyev <makhammadsoliyev@gmail.com>
2024-01-07 20:11:21 +0500

    Update

---
+++ FitnessTrackingSystem/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace FitnessTrackingSystem.Models;
+
+public class User
+{
+    private static int id = 0;
+
+    public User()
+    {
+        this.Id = ++id;   
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Height { get; set; }
+    public List<string> FitnessGoals { get; set; }
+    public int ExerciseExperienceMonths { get; set; }
+}
---
+++ FitnessTrackingSystem/Models/Workout.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessTrackingSystem.Models;
+
+public class Workout
+{
+    private static int id = 0;
+
+    public Workout()
+    {
+        this.Id = ++id;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<string> ExerciseSets { get; set; }
+    public int RestTime { get; set; } // minutes
+    public List<string> TargetedMuscleGroups { get; set; }
+}
