b08fe33803923740eb12e3c5504b47aa26c69a61
makhammadsoliyev <makhammadsoliyev@gmail.com>
2024-01-16 12:22:06 +0500

    Update

---
+++ ThreadWithFiles/Program.cs
@@ -0,0 +1,58 @@
+﻿using Spectre.Console;
+using System.Diagnostics;
+
+startLoop:
+try
+{
+    var file1 = AnsiConsole.Ask<string>("File1: ");
+    var thread1 = new Thread(
+    () => { OpenExplorerWithFile(file1); }
+    );
+
+    var file2 = AnsiConsole.Ask<string>("File2: ");
+    var thread2 = new Thread(
+        () => { OpenExplorerWithFile(file2); }
+    );
+
+    var file3 = AnsiConsole.Ask<string>("File3: ");
+    var thread3 = new Thread(
+        () => { OpenExplorerWithFile(file3); }
+    );
+
+    var file4 = AnsiConsole.Ask<string>("File4: ");
+    var thread4 = new Thread(
+        () => { OpenExplorerWithFile(file4); }
+    );
+
+    var file5 = AnsiConsole.Ask<string>("File5: ");
+    var thread5 = new Thread(
+        () => { OpenExplorerWithFile(file5); }
+    );
+
+    thread1.Start();
+    thread2.Start();
+    thread3.Start();
+    thread4.Start();
+    thread5.Start(); 
+    thread1.Join();
+    thread2.Join();
+    thread3.Join();
+    thread4.Join();
+    thread5.Join();
+}
+catch (Exception ex)
+{
+    // PDF
+    goto startLoop;
+    AnsiConsole.MarkupLine($"[red]{ex.Message}[/]");
+    Thread.Sleep(1500);
+}
+
+
+
+static void OpenExplorerWithFile(string filePath)
+{
+    Process.Start("explorer.exe", filePath);
+}
+
+
---
+++ VideoCutter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFilePath = "C:\\Users\\User\\Desktop\\dotnet\\HaadDotNetForSecondMonth\\VideoCutter\\Videos\\20 Sec Timer.mp4";
+        string outputFilePath = "output.mp4";
+        string startTime = "00:00:10";  // Start time in HH:mm:ss format
+        string duration = "00:00:05";   // Duration in HH:mm:ss format
+
+        CutVideo(inputFilePath, outputFilePath, startTime, duration);
+    }
+
+    static void CutVideo(string inputFilePath, string outputFilePath, string startTime, string duration)
+    {
+        Process process = new Process();
+        process.StartInfo.FileName = "ffmpeg.exe";  // Assuming ffmpeg is in the same directory as your application
+        process.StartInfo.Arguments = $"-i \"{inputFilePath}\" -ss {startTime} -t {duration} -c copy \"{outputFilePath}\"";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.CreateNoWindow = true;
+
+        process.Start();
+
+        process.WaitForExit();
+
+        if (process.ExitCode == 0)
+        {
+            Console.WriteLine("Video cut successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Error cutting video. Check the console output for more information.");
+        }
+    }
+}
+
